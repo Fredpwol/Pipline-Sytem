1a52bd21bbd2c8c8b194c2fe9e6a634fe8a0a877
Fredpwol <cyborg290@gmail.com>
2021-06-11 12:40:45 +0100

    added some fields

--- block/Block.js
+++ block/Block.js
@@ -2,49 +2,58 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 
 const BlockSchema = new mongoose.Schema({
-    previousBlockHash: {
-        type:String,
-        required: true,
-        min:1
-    },
-    PH: {
-        type: Number,
-        required: true
-    },
-    temperature: {
-        type: Number,
-        required: true
-    },
-    pressure: {
-        type: Number,
-        required: true
-    },
-    signature: String,
-    density: {
-        type: Number,
-        required: true
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now
-    }
-})
-
+  previousBlockHash: {
+    type: String,
+    required: true,
+    min: 1,
+  },
+  PH: {
+    type: Number,
+    required: true,
+  },
+  temperature: {
+    type: Number,
+    required: true,
+  },
+  pressure: {
+    type: Number,
+    required: true,
+  },
+  broadcaster: {
+    type: String,
+    required: true,
+  },
+  signature: String,
+  density: {
+    type: Number,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 class BlockClass {
+  get hash() {
+    var sha256 = crypto.createHash("sha256");
+    sha256.update(this.serializeBlock);
+    return sha256.digest("hex");
+  }
 
-    get hash(){
-        var sha256 = crypto.createHash("sha256");
-        sha256.update(this.serializeBlock);
-        return sha256.digest("hex");
-    }
-
-    get serializeBlock(){
-        return (JSON.stringify(Object.entries(this).sort()))
-    }
-
+  get serializeBlock() {
+    return JSON.stringify(
+      Object.entries({
+        previousBlockHash: this.previousBlockHash,
+        PH: this.PH,
+        density: this.density,
+        temperature: this.temperature,
+        pressure: this.pressure
+     }).sort()
+    );
+  }
 }
 
-BlockSchema.loadClass(BlockClass)
+BlockSchema.loadClass(BlockClass);
 
-module.exports = {BlockSchema, Block: mongoose.model("Block", BlockSchema)};
+module.exports = { BlockSchema, Block: mongoose.model("Block", BlockSchema) };
