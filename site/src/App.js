23e857d4d18bc633eb53128d8714428f380468a0
Fredpwol <cyborg290@gmail.com>
2021-06-11 12:42:15 +0100

    added routes and custom color palette

--- site/src/App.js
+++ site/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
+import "./styles/App.css"
+import SignUp from "./pages/auth/SignUp";
+import Home from "./pages/Home";
+import Copyright from "./components/Copyright"
+
+import { Box } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import cyan from "@material-ui/core/colors/cyan"
+import SignIn from "./pages/auth/Login";
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green[600],
+    },
+    secondary:{
+      main: cyan[600]
+    }
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="body">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={SignIn} />
+      </Switch>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </ThemeProvider>
   );
 }
 
