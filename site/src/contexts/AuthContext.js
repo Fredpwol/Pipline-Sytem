ac4a473251558e938483e045420d84789d11b041
Fredpwol <cyborg290@gmail.com>
2021-06-14 12:00:48 +0100

    auth context

---
+++ site/src/contexts/AuthContext.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+
+export const authContext = React.createContext();
+
+const AuthProvider = ({ children }) => {
+  const [organization, setOrganization] = useState("");
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+  const history = useHistory()
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"))
+    setEmail(localStorage.getItem("email") || "");
+    setOrganization(localStorage.getItem("org") || "");
+  },[])
+  
+  const logOut = () => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("email")
+    localStorage.removeItem("org")
+    setToken("")
+    setEmail("");
+    setOrganization("");
+    history.push("/login")
+  }
+
+  return (
+    <authContext.Provider
+      value={{
+        organization,
+        setOrganization,
+        email,
+        setEmail,
+        token,
+        setToken,
+        logOut
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
+
+export default AuthProvider;
