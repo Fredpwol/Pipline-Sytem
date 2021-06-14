22ccaeafa0844ffa00e4761e1868bb551753f402
Fredpwol <cyborg290@gmail.com>
2021-06-14 12:00:26 +0100

    added condtional rendering for auth and main route

--- site/src/App.js
+++ site/src/App.js
@@ -1,37 +1,51 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import "./styles/App.css"
+import "./styles/App.css";
 import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/Home";
-import Copyright from "./components/Copyright"
+import Copyright from "./components/Copyright";
 
 import { Box } from "@material-ui/core";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import green from "@material-ui/core/colors/green";
-import cyan from "@material-ui/core/colors/cyan"
+import cyan from "@material-ui/core/colors/cyan";
 import SignIn from "./pages/auth/Login";
-
+import { authContext } from "./contexts/AuthContext";
+import NavBar from "./components/NavBar";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: green[600],
     },
-    secondary:{
-      main: cyan[600]
-    }
+    secondary: {
+      main: cyan[600],
+    },
   },
 });
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const auth = useContext(authContext);
+  useEffect(() => {
+    setIsAuthenticated(Boolean(auth.token));
+  }, [auth.token]);
   return (
     <ThemeProvider theme={theme}>
+      <NavBar />
       <div className="body">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/login" component={SignIn} />
-      </Switch>
+        {!isAuthenticated ? (
+          <Switch>
+            <Route path="/login" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route render={() => <Redirect to="/login" />} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route render={() => <Redirect to="/" />} />
+          </Switch>
+        )}
       </div>
       <Box mt={5}>
         <Copyright />
