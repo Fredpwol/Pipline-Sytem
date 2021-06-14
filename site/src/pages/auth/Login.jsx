3b797eae9e8b5a79b854300f69fac5e3a31b8b0c
Fredpwol <cyborg290@gmail.com>
2021-06-14 11:59:51 +0100

    worked on login page

--- site/src/pages/auth/Login.jsx
+++ site/src/pages/auth/Login.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useContext, useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { authContext } from "../../contexts/AuthContext";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -35,7 +37,38 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn() {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
+  const [remember, setRemember] = useState(false)
+  const auth = useContext(authContext);
+  const history = useHistory()
 
+  const submit = () => {
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: new Headers({"Content-Type": "application/json"}),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status == "error") {
+          setErrors(data.message);
+          return
+        }
+        auth.setEmail(email)
+        auth.setToken(data.token)
+        console.log(data.token)
+        if(remember){
+          localStorage.setItem("token", data.token)
+        }
+        history.push("/")
+      })
+      .catch((err) => {
+        console.error(err);
+        setErrors(String(err));
+      });
+  };
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -46,16 +79,19 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
+        <Typography color="error">{errors}</Typography>
         <form className={classes.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
             required
             fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             id="email"
             label="Email Address"
             name="email"
-            autoComplete="email"
+            // autoComplete="email"
             autoFocus
           />
           <TextField
@@ -63,19 +99,21 @@ export default function SignIn() {
             margin="normal"
             required
             fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             name="password"
             label="Password"
             type="password"
             id="password"
-            autoComplete="current-password"
+            // autoComplete="current-password"
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
+            control={<Checkbox value={remember} color="primary" onChange={() => setRemember(!remember)} />}
             label="Remember me"
           />
           <Button
-            type="submit"
             fullWidth
+            onClick={submit}
             variant="contained"
             color="primary"
             className={classes.submit}
@@ -89,7 +127,7 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
