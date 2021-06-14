e8bb32004b376a19e65844ae170d24ef3e61f656
Fredpwol <cyborg290@gmail.com>
2021-06-14 14:19:56 +0100

    added cors middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 var mongoose = require("mongoose");
 const User = require("./user/User");
 const userRoute = require("./user");
@@ -12,6 +13,7 @@ const { longestValidChain } = require("./utils");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors())
 app.use("/users", userRoute);
 app.use("/blocks", blockRoute);
 
@@ -81,6 +83,7 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   try{
     const userExists = await User.findOne({ email: req.body.email });
+    console.log(req.body)
   if (!userExists) {
     return res
       .status(400)
