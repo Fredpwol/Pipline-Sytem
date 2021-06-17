420be696fdd7c0ab79af56af94d711167ebcb0d1
Fredpwol <cyborg290@gmail.com>
2021-06-17 14:56:05 +0100

    created 404 page

---
+++ site/src/pages/404.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@material-ui/core";
+
+
+export default function ErrorPage(){
+    return (
+        <div>
+            <Typography variant="h3">
+                404 Not Found!
+            </Typography>
+        </div>
+    )
+}
