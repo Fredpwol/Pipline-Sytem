88b36cdc4e6645939b78cc809b8ded5fa7a05e1e
Fredpwol <cyborg290@gmail.com>
2021-06-17 17:40:26 +0100

    displayed data

--- site/src/pages/BlockDisplay.jsx
+++ site/src/pages/BlockDisplay.jsx
@@ -1,15 +1,45 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Block from "../components/Block";
+import { authContext } from "../contexts/AuthContext";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+    content: {
+      paddingTop: "100px",
+    },
+    all:{
+      margin:"10px"
+    }
+  }));
 
-export default function BlockDisplay({route}){
-    useEffect(() => {
-        console.log(route.params._id)
-    } ,[])
+export default function BlockDisplay(){
+ const { _id } = useParams();
+ const [block, setBlock] = useState({});
+ const [errors, setErrors] = useState("")
+ const auth = useContext(authContext);
+ const classes = useStyles()
+
+ 
+
+ useEffect(() => {
+    Boolean(_id) && (fetch(`/blocks/${_id}`, {
+      headers: new Headers({ Authorization: `Bearer ${auth.token}` }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.status === "error") {
+          setErrors(data.message);
+        } else {
+          setBlock(data.block);
+        }
+      })
+      .catch((err) => setErrors(String(err))))
+  }, [_id]);
 
     return (
-        <div>
-            {route.params._id}
-            Hello World
+        <div className={classes.content}>
+            <Block block={block} title={`Block ${block._id} Created on ${new Date(block.timestamp).toLocaleString()}`} />
         </div>
     )
 }
