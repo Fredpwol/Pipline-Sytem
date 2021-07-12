bb597a627b12d90856635802631f410448576371
Fredpwol <fredpwol@gmail.com>
2021-07-12 14:25:32 +0100

    update broaadcast route

--- block/index.js
+++ block/index.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../user/User");
-const {
-  Block
-} = require("./Block");
+const { Block } = require("./Block");
 const {
   longestValidChain,
   isAuthenticated,
-  isBroadCaster
+  isBroadCaster,
 } = require("../utils");
 
 const router = express.Router();
 
-router.get("/broadcast", isBroadCaster ,async (req, res) => {
+router.get("/broadcast", isBroadCaster, async (req, res) => {
   // validattion later
   try {
     const broadcaster = await User.findOne({
-      _id: req.user._id
+      _id: req.user._id,
     });
     const users = await User.find({
-      isBroadCaster: false
+      isBroadCaster: false,
     });
     const longestChain = await longestValidChain();
     const block = {
-      ...req.query,
-      API_KEY: undefined
+      PH: req.query.PH,
+      density: req.query.density,
+      temperature: req.query.temperature,
+      flowRate: req.query.flowRate,
     };
     block.broadcaster = broadcaster.publicKey;
     console.log(longestChain);
@@ -43,7 +43,7 @@ router.get("/broadcast", isBroadCaster ,async (req, res) => {
     block.signature = broadcaster.signData(data);
     users.forEach(async (user) => {
       if (user.verifyChain() && user.chain.length === longestChain.length) {
-        await user.update({
+        await user.updateOne({
           $push: {
             chain: block,
           },
@@ -51,10 +51,10 @@ router.get("/broadcast", isBroadCaster ,async (req, res) => {
       } else {
         // update invalid chain to the longest valid chain
         user.set({
-          chain: longestChain
+          chain: longestChain,
         });
         await user.save();
-        await user.update({
+        await user.updateOne({
           $push: {
             chain: block,
           },
@@ -66,32 +66,38 @@ router.get("/broadcast", isBroadCaster ,async (req, res) => {
   } catch (e) {
     console.error(e);
     res.status(400).send({
-      message: String(e)
+      message: String(e),
     });
   }
 });
 
 router.get("/latest", isAuthenticated, async (req, res) => {
-  const user = await User.findById({
-    _id: req.user._id
-  });
-  if (!user) {
-    return res.status(403).json({
-      status: "error",
-      message: "user not found"
+  try{
+    const user = await User.findById({
+      _id: req.user._id,
     });
-  }
-  if (user.chain.length > 1) {
-    res
-      .status(200)
-      .json({
-        status: "ok",
-        block: user.chain[user.chain.length - 1]
+    if (!user) {
+      return res.status(403).json({
+        status: "error",
+        message: "user not found",
       });
-  } else {
-    res.status(200).json({
-      status: "ok",
-      block: {}
+    }
+    if (user.chain.length > 1) {
+      res.status(200).json({
+        status: "ok",
+        block: user.chain[user.chain.length - 1],
+      });
+    } else {
+      res.status(200).json({
+        status: "ok",
+        block: {},
+      });
+    }
+  }catch(error){
+    console.error(error)
+    res.status(400).json({
+      status: "error",
+      message:String(error),
     });
   }
 });
@@ -99,81 +105,73 @@ router.get("/latest", isAuthenticated, async (req, res) => {
 router.get("/", isAuthenticated, async (req, res) => {
   try {
     const user = await User.findById({
-      _id: req.user._id
+      _id: req.user._id,
     });
     if (!user) {
-      return res
-        .status(403)
-        .json({
-          status: "error",
-          message: "user not found"
-        });
+      return res.status(403).json({
+        status: "error",
+        message: "user not found",
+      });
     }
     res.status(200).json({
       status: "ok",
-      blocks: user.chain.reverse()
+      blocks: user.chain.reverse(),
     });
   } catch (err) {
     console.error(err);
     res.status(400).json({
       status: "error",
-      message: String(err)
+      message: String(err),
     });
   }
 });
 
 router.get("/:_id", isAuthenticated, async (req, res) => {
   const user = await User.findById({
-    _id: req.user._id
+    _id: req.user._id,
   });
   if (!user) {
     return res.status(403).json({
       status: "error",
-      message: "user not found"
+      message: "user not found",
     });
   }
 
   const block = user.chain.filter((data) => data._id == req.params._id);
   if (block.length < 1) {
-    return res
-      .status(404)
-      .json({
-        status: "error",
-        message: "Block not found"
-      });
+    return res.status(404).json({
+      status: "error",
+      message: "Block not found",
+    });
   }
   res.status(200).json({
     status: "ok",
-    block: block[0]
+    block: block[0],
   });
 });
 
 router.get("/timeline/:field", isAuthenticated, async (req, res) => {
-  const {
-    field
-  } = req.params;
+  const { field } = req.params;
   const aceptableFields = ["ph", "temperature", "flowrate", "density"];
   if (!aceptableFields.includes(field.toLowerCase())) {
-    return res
-      .status(400)
-      .json({
-        status: "error",
-        message: "Sorry enter a valid field!"
-      });
+    return res.status(400).json({
+      status: "error",
+      message: "Sorry enter a valid field!",
+    });
   }
   try {
     const user = await User.findById({
-      _id: req.user._id
+      _id: req.user._id,
     });
     const data = user.chain.map((block) => block[field]);
     res.status(200).json({
       status: "success",
-      data
+      data,
     });
   } catch (error) {
     return res.status(400).json({
       status: "error",
-      message: String(error)
+      message: String(error),
     });
   }
 });
