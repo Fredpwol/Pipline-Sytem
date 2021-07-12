576291919a8f8cedee34b616c21c1f404f4a8001
Fredpwol <fredpwol@gmail.com>
2021-07-12 14:26:28 +0100

    added spinner and empty container

--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from '@material-ui/core';
+import { Typography } from "@material-ui/core";
 import { authContext } from "../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import Loading from "@material-ui/core/CircularProgress";
 import Block from "../components/Block";
 import { API_URL } from "../utils";
 
@@ -10,22 +11,27 @@ const useStyles = makeStyles((theme) => ({
   content: {
     paddingTop: "100px",
   },
-  all:{
-    margin:"10px"
-  }
+  all: {
+    margin: "10px",
+  },
 }));
 
 export default function BasicTable() {
   const classes = useStyles();
   const auth = useContext(authContext);
-  const [block, setBlock] = useState({});
+  const [block, setBlock] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`${API_URL}/blocks/latest`, {
       headers: new Headers({ Authorization: `Bearer ${auth.token}` }),
     })
-      .then((res) => res.json())
+      .then((res) => {
+        setIsLoading(false);
+        return res.json();
+      })
       .then((data) => {
         if (data?.status === "error") {
           setErrors(data.message);
@@ -47,8 +53,22 @@ export default function BasicTable() {
         </div>
       ) : (
         <>
-        <Typography align="right" className={classes.all}><Link to="/blocks">See all</Link></Typography>
-          <Block block={block} title="Latest" />
+          <Typography align="right" className={classes.all}>
+            <Link to="/blocks">See all</Link>
+          </Typography>
+          {block !== null ? (
+            Object.entries(block).length !== 0 ? (
+              <Block block={block} title="Latest" />
+            ) : (
+              <center>
+                <h2>Sorry no block added yet.</h2>
+              </center>
+            )
+          ) : isLoading ? (
+            <center>
+              <Loading />
+            </center>
+          ) : null}
         </>
       )}
     </div>
